3baa0e29f333a783890271d6affb7b456c504c9b
Diegorr <diegoroblesrivera@gmail.com>
2024-04-17 20:09:48 -0400

    Ejercicios pytonn if

--- ProgramasPython/if.py
+++ ProgramasPython/if.py
@@ -18,18 +18,63 @@
 #     else:
 #         print("SAM solo es mayor que alex" )  
 
-num1=9
-num2=8
-num3=3
+# num1=9
+# num2=8
+# num3=3
 
-if num1>num2 and num1>num3:
-    print("el numero mayor es", num1)
-elif num2>num3:
-        print("el numero mayor es", num2)
+# if num1>num2 and num1>num3:
+#         print("el numero mayor es", num1)
+# elif num2>num3:
+#         print("el numero mayor es", num2)
+# else:
+#         print("el numero mayor es", num3) 
+
+
+# tacos=1
+# pizza=2
+# humitas=3
+# cazuela=4
+# print("Ejila una opcion de comida")
+# op=int(input())
+
+# if op==(1):
+#         print("Usted prefiere los Tacos")
+# elif(op==(2)):
+#         print("Usted prefiere la pizza")
+# elif(op==(3)):
+#         print("Usted prefiere las humitas")
+# elif(op==(4)):
+#         print("Usted prefiere la cazuela")
+# else:
+#         print("Usted es regodion")
+
+###ejemplo de variable boleana  ###
+# ticket_valido=False
+
+# folio_valido=123
+# print("Ingrese el numero de folio")
+# n_folio=int(input())
+
+# if n_folio==folio_valido:
+#         ticket_valido=True
+
+# if ticket_valido:
+#         print("este ticket és valido")
+# else:
+#         print("este ticket caducó")
+
+edad=9
+grupo="A"
+
+if edad>=18 and  grupo=="A":
+        print("Used es mayor y pertenece al grupo A")
+elif edad>=18 and  grupo=="B":
+        print("Used es mayor y pertenece al grupo B")
 else:
-        print("el numero mayor es", num3) 
+ print("Used no es mayor y no pertenece a ningun grupo")
 
 
+color="Azul"
 
-
-
+if color=="Azul" or  color=="Negro" or color=="Rojo":
+        print("Used tiene por lo menos un color")
--- for.py
+++ for.py
@@ -1,6 +1,13 @@
-num=0
-for i in range(3):
-    print("Ingrese un numero")
-    num=num+int(input())
+# num=0
+# for i in range(3):
+#     print("Ingrese un numero")
+#     num=num+int(input())
      
-print(num/3)
+# print(num/3)
+
+lista_numeros = [1, 3, 4, 5,6,7,8]
+
+for valor in lista_numeros:
+    if valor>3:
+        print("El numero ", valor, " es mayor que 3")
+            #print("Este valor es", valor)
