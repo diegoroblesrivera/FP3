821b44fe44645a84a8e76e29d1bc04e1822501b5
Diegorr <diegoroblesrivera@gmail.com>
2024-06-12 20:07:32 -0400

    Termino ejercicio

---
+++ Unidad3/Funciones/creadic.py
@@ -0,0 +1,23 @@
+from israel_solu import buscar_valor
+def creaDiccionario():
+    dic_vacio={}
+    while True:
+        key=input("Ingrese una key")
+        if key.lower()=="shazam":
+            print(dic_vacio)
+            break
+        else:
+            value=input("Ingrese el valor de key anterior")
+            dic_vacio[key] = value
+    preg=input("ingrese la key a buscar")
+    print(buscar_valor(dic_vacio, preg ))
+            
+
+creaDiccionario()
+"""Crear un programa cre cree un diccionario soliciando la key
+y el value de la misma. Seguira solicitando pares de datos
+hasta ingresar la palabra secreta SHAZAM. 
+Una vez creado el diccionario, debe llamar a la fucnion
+buscar_valor que se encuentra en el archivo israel_solu.py
+y mastrar el valor de la key que el usuario ingrese"""
+
--- Unidad3/Funciones/israel_solu.py
+++ Unidad3/Funciones/israel_solu.py
@@ -1,36 +1,37 @@
-def recorrer_lista(lista):
- for elemento in lista:
-    print(elemento)
-# Ejemplo de uso
+# def recorrer_lista(lista):
+#  for elemento in lista:
+#     print(elemento)
+# # Ejemplo de uso
 
-mi_lista = [1, 2, 3, 4, 5]
-# recorrer_lista(mi_lista)
+# mi_lista = [1, 2, 3, 4, 5]
+# # recorrer_lista(mi_lista)
 
-def pares_e_impares(lista):
+# def pares_e_impares(lista):
 
-    pares = []
-    impares = []
-    for elemento in lista:
-        if elemento % 2 == 0:
-            pares.append(elemento)
-        else:
-            impares.append(elemento)
-    print("Números pares:", pares)
-    print("Números impares:", impares)
-# Ejemplo de uso
-lista_completa = [1, 2, 3, 4, 5, 6, 7, 8, 9,66,88,23]
-# pares_e_impares(lista_completa)
+#     pares = []
+#     impares = []
+#     for elemento in lista:
+#         if elemento % 2 == 0:
+#             pares.append(elemento)
+#         else:
+#             impares.append(elemento)
+#     print("Números pares:", pares)
+#     print("Números impares:", impares)
+# # Ejemplo de uso
+# lista_completa = [1, 2, 3, 4, 5, 6, 7, 8, 9,66,88,23]
+# # pares_e_impares(lista_completa)
 
 def buscar_valor(diccionario, key):
-    if key in diccionario:
-        return diccionario[key]
-    else:
-        return f"La clave '{key}' no existe en el diccionario."
+    while True:
+        if key in diccionario:
+            return diccionario[key]
+        else:
+            return f"La clave '{key}' no existe en el diccionario."
 
-mi_diccionario = {'a': 1, 'b': 2, 'c': 3}
+# mi_diccionario = {'a': 1, 'b': 2, 'c': 3}
 
-clave_buscada = str(input("ingrese numero indice: "))
+# clave_buscada = str(input("ingrese numero indice: "))
 
-resultado = buscar_valor(mi_diccionario, clave_buscada)
+# resultado = buscar_valor(mi_diccionario, clave_buscada)
 
 # print(resultado)
