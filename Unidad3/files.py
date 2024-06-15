f0348ce552285ee7b5ae703f662ae28ac4fdffc0
Diegorr <diegoroblesrivera@gmail.com>
2024-06-15 11:16:38 -0400

    TRABAJANDO CON ARCHIVOS EN PYTHON

--- Unidad3/files.py
+++ Unidad3/files.py
@@ -1,3 +1,12 @@
+"""CREAR UN PROGRAMA QUE HAGA UNA TABLA DE 
+MULTIPLICAR Y LA GUARDE EN UN ARCHIVO
+EL ARCHIVO DEBE LLEVAR EL NUMERO DE LATABLA
+QUE SE INTRODUJO POR EL USUARIO
+EJ: TABLA5.TXT"""
+
 num=int(input())
+
 for i in range(1,11):
-    print(num ," x ", i , " = ", num*i )
+    with open(f'tablaDel{num}.txt', 'a') as archivo:
+        archivo.write(str(num) +" x "+ str(i) + " = "+ str(num*i)+"\n" )
+    
