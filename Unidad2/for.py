ff062fb27d46bf4901015141d19e71390225cbd2
Droblesr <diegoroblesrivera@gmail.com>
2025-01-08 21:49:49 -0300

    ejecicos for

--- Unidad2/for.py
+++ Unidad2/for.py
@@ -1,3 +1,40 @@
+
+# for j in range(10):
+#     for i in range(10):
+#      print(j+1 ,"x",i+1,"=",(i+1)*(j+1))
+     
+
+
+
+
+# total=0    
+# for i in range(3):
+#     print("Ingrese una nota")
+#     nota=int(input())
+#     total=total+nota
+# print("Su promedio es ",total/(i+1))
+   
+# num=int(input("Ingrese un numero"))    
+# for i in range(num+1):
+#     if i%2!=0:
+#         print("El numero es  no es par", i)
+
+# pal="Hola soy milo"
+
+# print(len(pal))
+
+# for i in pal:
+#     print(i)
+    
+# total=0
+# nom=input()
+# for i in range(len(nom)):
+#     total=total+(i+1)
+# print("El total de la suma de sus numeros es ", total)   
+
+ 
+
+
 # num=0
 # for i in range(3):
 #     print("Ingrese un numero")
@@ -5,9 +42,21 @@
      
 # print(num/3)
 
+passw=123
+
+for i in range(3):
+    pas=int(input("Ingrese una contraseña"))
+    if pas==passw:
+        print("Bienvenido a Skynet")
+        break
+    else:
+        print("Ha fallado intente nuevamente")
+# print("No ha podiso ingresar, autodestruccion")
+
 # lista_numeros = [55, 79, 4, 5,6,7,8]
 
 # for valor in lista_numeros:
+#     print(valor)
 #     if valor>3:
 #         print("El numero ", valor, " es mayor que 3")
 #             #print("Este valor es", valor)
